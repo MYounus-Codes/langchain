27b98187b60e644f2105b504babaa3b48cc5818c
MYounus-Codes <myounushere@gmail.com>
2025-12-07 08:14:26 +0500

    some changes are made in 1_llm_demo.py in llm folder

--- components/models/llms/1_llm_demo.py
+++ components/models/llms/1_llm_demo.py
@@ -11,3 +11,12 @@ result = llm.invoke("What is the capital of Pakistan?")
 print(result)
 
 ### Output:  The capital of Pakistan is Islamabad. ###
+
+## With minor changes, we can use other LLMs as well.
+
+from langchain_google_genai import GoogleGenerativeAI
+
+llm_gemini = GoogleGenerativeAI(model="gemini-2.5-flash", temperature=0.3)
+result_gemini = llm_gemini.invoke("What is the capital of Pakistan?")
+
+print(result_gemini.content)
