d7c9ab4c889123567cd61b6492196ac00c8043f4
MYounus-Codes <myounushere@gmail.com>
2025-12-07 08:06:39 +0500

    Code of gemini file is improved by providing some more parameters to ChatGoogleGenerativeAI

--- components/models/chatmodels/1_chatmodel_gemini.py
+++ components/models/chatmodels/1_chatmodel_gemini.py
@@ -2,7 +2,10 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 load_dotenv()  # Load environment variables from a .env file
 
-model = ChatGoogleGenerativeAI(model='gemini-2.5-flash')
+# model = ChatGoogleGenerativeAI(model='gemini-2.5-flash') ## Default temperature is 0.0
+model = ChatGoogleGenerativeAI(model='gemini-2.5-flash', temperature=0.3)  
+# Temperature is use to control the randomness of the output
+# You can also provide the max_completions_tokens parameter to limit the response length
 response = model.invoke("What is the capital of Pakistan?")
 # print(response) # Printing the full response
 print(response.content) # Printing only the content of the response
