37d112b05cb6c999172a157c31113f7ef70e8572
MYounus-Codes <myounushere@gmail.com>
2025-12-08 11:13:24 +0500

    Now the history is added to the chatbot

--- components/prompts/class_projects/cli_chatbot.py
+++ components/prompts/class_projects/cli_chatbot.py
@@ -5,14 +5,21 @@ load_dotenv()
 
 model = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
-    api_key=os.getenv("GOOGLE_GENAI_API_KEY"),
+    # api_key=os.getenv("GOOGLE_GENAI_API_KEY"),
+    api_key="AIzaSyDBoxmlD1e8NJ8aUPzKRd6INRQGzLRkdzs",
 )
+
+chat_history = []
 
 while True:
     user = input("You: ")
+    chat_history.append({"role": "user", "content": user})
     if user.lower() in ["exit", "quit"]:
         print("Exiting chat...")
         break
-    response = model.invoke(user)
+    response = model.invoke(chat_history)
+    chat_history.append({"role": "assistant", "content": response.content})
     print("AI:", response.content)
-    
+
+print("Chat ended.")
+print("Final chat history:", chat_history)
